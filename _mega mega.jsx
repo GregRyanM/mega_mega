d4f8a5b2e72af1f261df591d1dc62af6bfa06cf6
GregRyanM <130040691+GregRyanM@users.noreply.github.com>
2023-04-06 11:40:37 +0100

    Add files via upload

--- _mega mega.jsx
+++ _mega mega.jsx
@@ -7,17 +7,17 @@ if (sourceFolder != null) {
   // Get all files in the source folder
   var files = sourceFolder.getFiles("*.jpg");
 
-  // Create a folder for completed files
-  var completedFolder = new Folder(sourceFolder + "/_completed");
-  if(!completedFolder.exists){
-    completedFolder.create();
-  }
 
   // Loop through each file
   for (var i = 0; i < files.length; i++) {
 
     // Get the current file
     var sourceFile = files[i];
+
+// Open the source file
+      var sourceDoc = app.open(sourceFile);
+
+    
 
     // Get the path to the output file
     var outputFilePath = "/Users/" + $.getenv("USER") + "/Documents/Elvis Checkouts/" + sourceFile.name;
@@ -27,9 +27,6 @@ if (sourceFolder != null) {
     if (outputFile.exists) {
       // Open the output file
       app.open(outputFile);
-
-      // Open the source file
-      var sourceDoc = app.open(sourceFile);
 
       // Switch to the source file tab
       app.activeDocument = sourceDoc;
@@ -55,30 +52,12 @@ if (sourceFolder != null) {
       app.activeDocument = sourceDoc;
     }
 
-    // Switch to the target document to continue copying paths
-    app.activeDocument = targetDoc;
-
-    for (var i = 0; i < targetDoc.pathItems.length; i++) {
-      targetDoc.pathItems[i].select();
-      var idcopy = charIDToTypeID( "copy" );
-      executeAction( idcopy, undefined, DialogModes.NO );
-
-      app.activeDocument = sourceDoc;
-      app.doAction("Deselect current path", "for script"); // Run "Deselect current path" action
-      var idpast = charIDToTypeID( "past" );
-      executeAction( idpast, undefined, DialogModes.NO );
-
-      app.activeDocument = targetDoc;
-    }
 
     // Close the active document
     app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
 
     // Run "save n close" action
     app.doAction("save n close", "for script");
-
-    // Move the source file to the completed folder
-    sourceFile.rename(completedFolder + "/" + sourceFile.name);
   }
 } else {
   alert("You didn't select any folder. Please run the script again.");
